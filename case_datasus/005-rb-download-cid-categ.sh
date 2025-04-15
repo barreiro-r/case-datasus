0dd9fca4c124363a55c479e523eb1e8534aa27e3
barreiro-r <rodrigoasbarreiro@gmail.com>
2025-04-15 20:44:14 -0300

    add motivos-saida

---
+++ case_datasus/005-rb-download-cid-categ.sh
@@ -0,0 +1,4 @@
+# Download Manual de Bases Técnicas - Oncologia
+curl -L \
+    -o data/external/CID-10-CATEGORIAS.CSV.utf8 \
+    https://github.com/bigdata-icict/ETL-Dataiku-DSS/raw/refs/heads/master/SIM/CID-10-CATEGORIAS.CSV.utf8
--- case_datasus/005-rb-download-cid-subcateg.sh
+++
@@ -1,6 +0,0 @@
-# Download Manual de Bases Técnicas - Oncologia
-curl -L \
-    -o data/external/CID-10-SUBCATEGORIAS.CSV.utf8 \
-    https://github.com/bigdata-icict/ETL-Dataiku-DSS/raw/refs/heads/master/SIM/CID-10-SUBCATEGORIAS.CSV.utf8
-
-    https://github.com/leogermani/estados-e-municipios-ibge/raw/refs/heads/master/estados.csv
